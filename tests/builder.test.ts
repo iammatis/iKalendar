88dd9043b68db6a89eb285d9218886c732e9bb74
Matej <matej.vilk@gmail.com>
2021-04-14 14:54:01 +0200

    Add #72 console log to test

--- tests/builder.test.ts
+++ tests/builder.test.ts
@@ -280,7 +280,10 @@ describe('Test Builder Class', () => {
 				]
 			})
 
+			
 			const data = builder.build()
+			console.log({file})
+			console.log({data})
 
 			expect(data).toEqual(file)
 		})
